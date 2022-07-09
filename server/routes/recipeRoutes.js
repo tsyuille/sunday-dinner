d26040d542e815ebecc118816effcdc8c9e8d993
TaVon <tsyuille@gmail.com>
2022-07-09 11:41:52 -0400

    Removed unused code

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const recipeController = require('../controllers/recipeController')
 const authController = require('../controllers/auth')
-// const homeController = require('../controllers/home')
 const { ensureAuth, ensureGuest } = require('../../middleware/auth')
 
 // App Routes
