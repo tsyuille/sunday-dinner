87d71b540417729d8d0ed4350e1b8400a68a7f34
TaVon <tsyuille@gmail.com>
2022-07-09 11:30:57 -0400

    Removed unused code

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -40,6 +40,6 @@ const recipeSchema = new mongoose.Schema({
       }
 })
 
-recipeSchema.index({ name: 'text' /*description: 'text'*/ })
+recipeSchema.index({ name: 'text' })
 
 module.exports = mongoose.model('Recipe', recipeSchema)
