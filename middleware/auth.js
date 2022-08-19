74d2f7de612bbcbe96f850bb59430780d6d107ff
TaVon <tsyuille@gmail.com>
2022-08-18 20:38:02 -0400

    Fix auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,16 @@
 module.exports = {
     ensureAuth: function (req, res, next) {
       if (req.isAuthenticated()) {
-        return next();
+        return next()
       } else {
         res.redirect('/home')
       }
     },
     ensureGuest: function (req, res, next) {
-      if (!req.isAuthenticated()) {
-        return next()
+      if (req.isAuthenticated()) {
+        res.redirect('/home')
       } else {
-        res.redirect('/')
+        return next()
       }
     }
   }
