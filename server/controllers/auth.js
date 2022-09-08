a0c1ca0185fd5cd1d7cfaa4d159d33cccc703427
TaVon <tsyuille@gmail.com>
2022-09-08 02:00:51 -0400

    Update logout callback function

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -44,15 +44,14 @@ exports.postLogin = (req, res, next) => {
   })(req, res, next)
 }
 
-exports.logout = (req, res) => {
-  req.logout()
-  req.session.destroy((err) => {
-    if (err)
-      console.log("Error : Failed to destroy the session during logout.", err)
-    req.user = null
+exports.logout = (req, res, next) => {
+  req.logout(function(err) {
+    if (err) {
+      return next(err)
+    }
     res.redirect('/')
-  })
-}
+    })  
+  }
 
 exports.getSignup = (req, res) => {
   if (req.user) {
@@ -108,7 +107,7 @@ exports.postSignup = (req, res, next) => {
           if (err) {
             return next(err)
           }
-          res.redirect('/home')
+          res.redirect('/')
         })
       })
     }
